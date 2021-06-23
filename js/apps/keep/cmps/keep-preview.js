e5e6aa0cda8045b3022744626ba451613245c5d2
MoriaNuri <moriakorsia@gmail.com>
2021-06-23 16:16:19 +0300

    keep-service,app,preview,list

---
+++ js/apps/keep/cmps/keep-preview.js
@@ -0,0 +1,12 @@
+export default {
+    props: ['note'],
+    template: `
+    <section class="keep preview">
+       <component :is="note.type" :is="note" class="note-content" />
+       {{note}}
+    </section>
+    
+    `,
+
+
+}
