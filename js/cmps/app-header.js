09c20e66010809fe29c76cf30ea45ba2beb5ee3e
Hanan Neor <hananneor@gmail.com>
2021-06-27 02:47:54 +0300

    home page

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -2,11 +2,11 @@ export default {
     template: `
     <header class="app-header flex space-between align-center">
 
-        <router-link to="/" class= "header-logo">Appsus</router-link>
+        <router-link to="/" class= "header-logo"><img src="../img/logo.png"/></router-link>
         <nav>
                 <router-link to="/email/email-list">Email</router-link> |
                 <router-link to="/keep">Keep</router-link> |
-                <a href="https://hanan-neor.github.io/books-vue/">Book</a>
+                <a href="https://hanan-neor.github.io/books-vue/#/book">Book</a>
         </nav> 
     </header>
     `,
--- js/pages/home-page.js
+++ js/pages/home-page.js
@@ -1,7 +1,15 @@
  export default {
      template: `
-    <section class="home-page-app main-layout">
-        <div class="home-page">
+    <section class="home-page main-layout flex">
+        <div class="welcome">
+            <div class="logo">
+              <img src="../img/your productive app (1).png"/>
+            </div>
+            <div class="links flex">
+        <a href="https://hanan-neor.github.io/books-vue/#/book"><div><img src="../img/2 (2).png"/></div></a>
+            <router-link to="/keep"><div><img src="../img/4 (2).png"/></div></router-link>
+            <router-link to="/email/email-list"><div><img src="../img/1 (2).png"/></div></router-link>
+ </div>
         </div>
     </section>
     `
