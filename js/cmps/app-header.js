c5d70e5a83ad98e89358de93b21a84d04fc065fe
Hanan Neor <hananneor@gmail.com>
2021-06-27 05:11:41 +0300

    menu

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -3,12 +3,29 @@ export default {
     <header class="app-header flex space-between align-center">
 
         <router-link to="/" class= "header-logo"><img src="../img/logo.png"/></router-link>
-        <nav>
-                <router-link to="/email/email-list" >Email</router-link> |
-                <router-link to="/keep">Keep</router-link> |
-                <a href="https://hanan-neor.github.io/books-vue/#/book">Book</a>
+        <i @click="togglingMenu" style="cursor:pointer" class="fas fa-bars"></i>
+        <nav v-show="toggleMenu">
+        <a href="https://hanan-neor.github.io/books-vue/#/book"><div @click="togglingMenu"><img src="./img/books.png"/></div></a>
+            <router-link to="/keep"><div @click="togglingMenu"><img src="./img/notes.png" /></div></router-link>
+            <router-link to="/email/email-list"><div  @click="togglingMenu"><img src="./img/email.png"/></div></router-link>
         </nav> 
     </header>
     `,
-    components: {}
+    data() {
+        return {
+            toggleMenu: false,
+        }
+    },
+    methods: {
+        togglingMenu() {
+            this.toggleMenu = !this.toggleMenu;
+        }
+    },
+    // mounted() {
+    //     document.addEventListener('click', this.close)
+    // },
+    // beforeDestroy() {
+    //     document.removeEventListener('click', this.close)
+    // },
+    // components: {}
 }
