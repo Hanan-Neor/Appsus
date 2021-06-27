cbd37e85c2424529c8b84efa7141ff37e6a2b54c
Hanan Neor <hananneor@gmail.com>
2021-06-27 03:57:23 +0300

    -

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -2,7 +2,7 @@ export default {
     template: `
     <header class="app-header flex space-between align-center">
 
-        <router-link to="/" class= "header-logo"><img src="./../img/logo.png"/></router-link>
+        <router-link to="/" class= "header-logo"><img src="../img/logo.png"/></router-link>
         <nav>
                 <router-link to="/email/email-list" >Email</router-link> |
                 <router-link to="/keep">Keep</router-link> |
