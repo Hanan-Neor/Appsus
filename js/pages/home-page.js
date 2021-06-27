462ddd47db34ed9d7786cb6b81c94fea4c230cd9
Hanan Neor <hananneor@gmail.com>
2021-06-27 03:10:26 +0300

    -

--- js/pages/home-page.js
+++ js/pages/home-page.js
@@ -3,12 +3,12 @@
     <section class="home-page main-layout flex">
         <div class="welcome">
             <div class="logo">
-              <img src="../img/logo-homepage.png"/>
+              <img src="./img/logo-homepage.png"/>
             </div>
             <div class="links flex">
-        <a href="https://hanan-neor.github.io/books-vue/#/book"><div><img src="../img/books.png"/></div></a>
-            <router-link to="/keep"><div><img src="../img/notes.png"/></div></router-link>
-            <router-link to="/email/email-list"><div><img src="../img/email.png"/></div></router-link>
+        <a href="https://hanan-neor.github.io/books-vue/#/book"><div><img src="./img/books.png"/></div></a>
+            <router-link to="/keep"><div><img src="./img/notes.png"/></div></router-link>
+            <router-link to="/email/email-list"><div><img src="./img/email.png"/></div></router-link>
  </div>
         </div>
     </section>
